78e5c1cd6e36a0f7f21b854e0f7a829aac323acc
Aidan-Jared <46360020+Aidan-Jared@users.noreply.github.com>
2019-03-10 10:31:42 -0600

    added in spacy and fixed Read_Data to extract 1%

--- src/ContentID.py
+++ src/ContentID.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import pyarrow.parquet as pq
+import spacy
 from sklearn.feature_extraction import text
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.decomposition import LatentDirichletAllocation
@@ -9,9 +11,12 @@ from wordcloud import WordCloud
 
 if __name__ == "__main__":
     #importing files and making the main corpus to work with
-    df = pd.read_csv('data/Amz_book_review_short.csv')
-    df.dropna(inplace=True)
-    corpus = df['reveiw_body'].values
+    table = pq.read_table('data/Amz_book_review_short.parquet')
+    df = table.to_pandas()
+    corpus = df.review_body.values
 
-    stopwords = text.ENGLISH_STOP_WORDS
-    vect = CountVectorizer(max_features=500, max_df=.85, min_df=2, stop_words=stopwords)
+    nlp = spacy.load('en_core_web_sm')
+    doc = nlp(corpus)
+    # stopwords = text.ENGLISH_STOP_WORDS
+    # vect = CountVectorizer(max_features=500, max_df=.85, min_df=2, stop_words=stopwords)
+    # vect.fit(corpus)
